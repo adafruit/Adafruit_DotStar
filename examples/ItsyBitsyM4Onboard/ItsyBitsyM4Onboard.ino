7f4c356625b12ed9ad29a452c6ed6425a20dd368
Ladyada <support@adafruit.com>
2019-08-04 18:41:29 -0400

    less confusing example

--- examples/ItsyBitsyM4Onboard/ItsyBitsyM4Onboard.ino
+++ examples/ItsyBitsyM4Onboard/ItsyBitsyM4Onboard.ino
@@ -1,6 +1,5 @@
 // An example demonstrating how to control the Adafruit Dot Star RGB LED
 // included on board the ItsyBitsy M4 board.
-// This randomly shifts the red, green and blue brightness
 
 #include <Adafruit_DotStar.h>
 
@@ -11,21 +10,40 @@
 #define DATAPIN    8
 #define CLOCKPIN   6
 
-Adafruit_DotStar px(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
+Adafruit_DotStar strip(NUMPIXELS, DATAPIN, CLOCKPIN, DOTSTAR_BRG);
 
 void setup() {
-  px.begin(); // Initialize pins for output
-  px.show();  // Turn all LEDs off ASAP
-  randomSeed(analogRead(0)); //initialise the random number generator
+  strip.begin(); // Initialize pins for output
+  strip.setBrightness(80);
+  strip.show();  // Turn all LEDs off ASAP
 }
 
-uint16_t red = 0, green = 0, blue = 0;
-
 void loop() {
-  px.setPixelColor(0, red, green, blue); // Set the pixel colors
-  px.show();              // Refresh strip
-  delay(20);              // Pause 20 milliseconds (~50 FPS)
-  red   += random(-8, 8); // add random numbers to each pixel value
-  green += random(-8, 8);
-  blue  += random(-8, 8);
+  rainbow(10);             // Flowing rainbow cycle along the whole strip
+}
+
+
+
+// Rainbow cycle along whole strip. Pass delay time (in ms) between frames.
+void rainbow(int wait) {
+  // Hue of first pixel runs 5 complete loops through the color wheel.
+  // Color wheel has a range of 65536 but it's OK if we roll over, so
+  // just count from 0 to 5*65536. Adding 256 to firstPixelHue each time
+  // means we'll make 5*65536/256 = 1280 passes through this outer loop:
+  for(long firstPixelHue = 0; firstPixelHue < 5*65536; firstPixelHue += 256) {
+    for(int i=0; i<strip.numPixels(); i++) { // For each pixel in strip...
+      // Offset pixel hue by an amount to make one full revolution of the
+      // color wheel (range of 65536) along the length of the strip
+      // (strip.numPixels() steps):
+      int pixelHue = firstPixelHue + (i * 65536L / strip.numPixels());
+      // strip.ColorHSV() can take 1 or 3 arguments: a hue (0 to 65535) or
+      // optionally add saturation and value (brightness) (each 0 to 255).
+      // Here we're using just the single-argument hue variant. The result
+      // is passed through strip.gamma32() to provide 'truer' colors
+      // before assigning to each pixel:
+      strip.setPixelColor(i, strip.gamma32(strip.ColorHSV(pixelHue)));
+    }
+    strip.show(); // Update strip with new contents
+    delay(wait);  // Pause for a moment
+  }
 }
