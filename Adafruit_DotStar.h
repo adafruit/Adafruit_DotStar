82551c28ebe08aceec05fe76a74ffa96ea2b7082
John C. Westmoreland <john@westmorelandengineering.com>
2020-09-14 17:34:59 -0700

    Using Portenta-H7 shouldn't be the default --- jcw

--- Adafruit_DotStar.h
+++ Adafruit_DotStar.h
@@ -28,7 +28,8 @@
 #include <pins_arduino.h>
 #endif
 
-#define PORTENTA_H7	// STM32H747xI
+// Uncomment the following to use the Portanta-H7 with the MKR-RGB Shield
+// #define PORTENTA_H7	// STM32H747xI
 
 // Color-order flag for LED pixels (optional extra parameter to constructor):
 // Bits 0,1 = R index (0-2), bits 2,3 = G index, bits 4,5 = B index
