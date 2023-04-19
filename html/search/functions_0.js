6f724a10476fbc9004d935fa0183187523f5660f
Doxygen CI <ci-arduino@invalid>
2023-04-19 16:56:40 +0000

    Deploy docs to GitHub Pages from commit a65dbdba07
    Commit: a65dbdba07897c3922284490ce87a91bb40d0e02
    GitHub Actions run: 4745873943

--- html/search/functions_0.js
+++ html/search/functions_0.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['adafruit_5fdotstar',['Adafruit_DotStar',['../class_adafruit___dot_star.html#a23a2fff3ccbd84ef48348c713bcf63e0',1,'Adafruit_DotStar::Adafruit_DotStar(uint16_t n, uint8_t o=DOTSTAR_BRG, SPIClass *spi=NULL)'],['../class_adafruit___dot_star.html#a2fce3fd6bfe6e5caaa7a1308197d638b',1,'Adafruit_DotStar::Adafruit_DotStar(uint16_t n, uint8_t d, uint8_t c, uint8_t o=DOTSTAR_BRG)']]]
+  ['adafruit_5fdotstar',['Adafruit_DotStar',['../class_adafruit___dot_star.html#af651211fd0288e339e6a3806845c62d9',1,'Adafruit_DotStar::Adafruit_DotStar(uint16_t n, uint8_t o=DOTSTAR_BRG, SPIClass *spi=&amp;SPI)'],['../class_adafruit___dot_star.html#a2fce3fd6bfe6e5caaa7a1308197d638b',1,'Adafruit_DotStar::Adafruit_DotStar(uint16_t n, uint8_t d, uint8_t c, uint8_t o=DOTSTAR_BRG)']]]
 ];
